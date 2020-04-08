3ba0d73628d42f2f47a95c8fcdfe2c3ea5957afe
shwetank-rai <62825179+shwetank-rai@users.noreply.github.com>
2020-04-08 17:42:28 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,6 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+window.alert("Web site will update soon not added JavaScript ☹");
